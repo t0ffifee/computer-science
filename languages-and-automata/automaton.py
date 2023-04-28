100eee519fe88ae9ad3468c71104162ddf9c6caa
Toufik Ammoura <toufikammoura@gmail.com>
2023-04-28 12:05:21 +0200

    work

--- languages-and-automata/automaton.py
+++ languages-and-automata/automaton.py
@@ -1,10 +1,36 @@
 
+"""
+asteriks next to state means state is final
+
+language: uneven amount of a's and must end with b
+input:
+states: a b c*
+transitions:
+a -> "a" -> b
+a -> "b" -> a
+b -> "a" -> a
+b -> "b" -> c
+"""
+
+class state:
+  def __init__(self, is_final):
+    self.is_final = is_final
+
+class transition:
+  def __init__(self, begin, change, end):
+    self.begin = begin
+    self.change = change
+    self.end = end
+
 class automaton:
   """
   states:
   transitions:
-  final_states:
   """
+  def __init__(self, states, transitions):
+    self.states = states
+    self.transitions = transitions
+
 
   def to_regex():
     pass
